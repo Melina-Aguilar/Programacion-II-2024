8687536acfeabfe74d50bcb2b774b28d54b0e271
Melina Aguilar <aguilarmelinaelizabeth@gmail.com>
2024-09-24 18:41:46 -0300

    Agrego clase 5 de Portfolio

--- PortfolioVuejs/src/App.vue
+++ PortfolioVuejs/src/App.vue
@@ -1,5 +1,7 @@
 <script setup> // setup: indica que se va a ejecutar solamente el codigo que se encuentra dentro de este script
   import NavBar from './components/icons/NavBar.vue';
+  import DatosPersonales from './components/DatosPersonales.vue';
+  import Educacion from './components/Educacion.vue';
 </script>
 
 <template>
@@ -8,15 +10,21 @@
     <!-- Barra de Navegacion -->
      <NavBar />
 
+    <!-- Componente Datos Persoanles-->
+      <DatosPersonales />
   </header>
 
   <main>
-      <section id="educacion"><h2>Educacion - Cursos</h2></section>
+    <!-- El estilo css de section se encuentra en base.css-->
+      <section id="educacion"><h2>Educacion - Cursos</h2>
+      <Educacion/>
+      </section>
       <section id="experiencia"><h2>Experiencia</h2></section>
       <section id="proyectos"><h2>Proyectos</h2></section>
       <section id="habilidades"><h2>Habilidades</h2></section>
       <section id="intereses"><h2>Intereses</h2></section>
   </main>
+  
 
 </template>
 
