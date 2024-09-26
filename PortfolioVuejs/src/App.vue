3fde6342ee74e671effac64ebfa02fdfbeea8980
Melina Aguilar <aguilarmelinaelizabeth@gmail.com>
2024-09-26 17:00:19 -0300

    Agrego clase 7 de PortfolioVuejs

--- PortfolioVuejs/src/App.vue
+++ PortfolioVuejs/src/App.vue
@@ -1,7 +1,8 @@
 <script setup> // setup: indica que se va a ejecutar solamente el codigo que se encuentra dentro de este script
   import NavBar from './components/icons/NavBar.vue';
   import DatosPersonales from './components/DatosPersonales.vue';
-  import Educacion from './components/Educacion.vue';
+  import Educacion from './components/EducacionComponente.vue';
+  import ExperienciaComponente from './components/ExperienciaComponente.vue';
 </script>
 
 <template>
@@ -19,7 +20,9 @@
       <section id="educacion"><h2>Educacion - Cursos</h2>
       <Educacion/>
       </section>
-      <section id="experiencia"><h2>Experiencia</h2></section>
+      <section id="experiencia"><h2>Experiencia</h2>
+      <ExperienciaComponente/>
+      </section>
       <section id="proyectos"><h2>Proyectos</h2></section>
       <section id="habilidades"><h2>Habilidades</h2></section>
       <section id="intereses"><h2>Intereses</h2></section>
