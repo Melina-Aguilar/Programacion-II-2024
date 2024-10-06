2b41c883f4bcbf9399100c1daa0f8c6b73cce268
Melina Aguilar <aguilarmelinaelizabeth@gmail.com>
2024-10-06 12:32:02 -0300

    Agrego clase 8 de Vuejs

--- PortfolioVuejs/src/App.vue
+++ PortfolioVuejs/src/App.vue
@@ -3,6 +3,7 @@
   import DatosPersonales from './components/DatosPersonales.vue';
   import Educacion from './components/EducacionComponente.vue';
   import ExperienciaComponente from './components/ExperienciaComponente.vue';
+  import ProyectosComponente from './components/ProyectosComponente.vue';
 </script>
 
 <template>
@@ -18,12 +19,20 @@
   <main>
     <!-- El estilo css de section se encuentra en base.css-->
       <section id="educacion"><h2>Educacion - Cursos</h2>
+      <!--Componente Educacion-->
       <Educacion/>
       </section>
+
       <section id="experiencia"><h2>Experiencia</h2>
+      <!--Componente Experiencia-->
       <ExperienciaComponente/>
       </section>
-      <section id="proyectos"><h2>Proyectos</h2></section>
+
+      <section id="proyectos"><h2>Proyectos</h2>
+      <!--Componente Proyectos-->
+      <ProyectosComponente />
+      </section>
+
       <section id="habilidades"><h2>Habilidades</h2></section>
       <section id="intereses"><h2>Intereses</h2></section>
   </main>
