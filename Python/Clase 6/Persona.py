524b25f55a068d05e8cd27fc3ba7936f53ec2eed
Melina Aguilar <aguilarmelinaelizabeth@gmail.com>
2024-10-07 16:00:15 -0300

    Agrego Clase 7 de Python

--- Python/Clase 6/Persona.py
+++ Python/Clase 6/Persona.py
@@ -1,13 +1,17 @@
 class Persona: # Creamos una clase
 
-    def __init__(self, nombre, apellido, edad):  # Se lo llama metodo Init Dunder
+    def __init__(self, nombre, apellido, dni, edad, *args, **kwargs):  # Se lo llama metodo Init Dunder
         self.nombre = nombre
         self.apellido = apellido
+        self._dni = dni  #Este atributo esta encapsulado de una manera sugerida
         self.edad = edad
-    def mostrar_detalle(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+        self.args = args
+        self.kwargs = kwargs
+        
+    def mostrar_detalle(self):  # self es igual a this.
+        print(f'La clase Persona tiene los siguientes datos: {self.nombre} {self.apellido} {self._dni} {self.edad}, la direccion es: {self.args}, los datos son: {self.kwargs}')
 
-persona1 = Persona('Ariel', 'Betancud', 40)  # Necesitamos enviar argumentos
+persona1 = Persona('Ariel', 'Betancud', 34242331, 40)  # Necesitamos enviar argumentos
 print(persona1.nombre)      # TAREA: Hacer el print igual que con el objeto 2
 print(persona1.apellido)
 print(persona1.edad)
@@ -15,7 +19,7 @@ print(persona1.edad)
 #TAREA
 print(f'El objeto1 de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}')
 
-persona2 = Persona('Osvaldo', 'Giordanini', 45)
+persona2 = Persona('Osvaldo', 'Giordanini', 2223131, 45)
 print(f'El objeto2 de la clase persona: {persona2.nombre} {persona2.apellido} Su edad es: {persona2.edad}')
 
 persona1.nombre = 'Liliana'
@@ -26,5 +30,14 @@ print(f'El objeto1 modificado de la clase persona: {persona1.nombre} {persona1.a
 
 # Los atributos son caracteristicas
 # Los metodos son el comportamiento que van a tener los objetos (acciones)
-persona1.mostrar_detalle()
+persona1.mostrar_detalle() # la referencia en estos casos se pasan de manera automatica
 persona2.mostrar_detalle()
+
+# Persona.mostrar_detalle(persona1) #Debemos pasarle una referencia para el self o dara error
+persona1.telefono = '232421313'
+print(f'Este es el telefono de: {persona1.nombre} {persona1.telefono}') # Hemos creado un atributo de un objeto
+
+# print(persona2.telefono)  # el objeto  'persona2' no tiene este atributo, da error
+
+persona3 = Persona('Rogelio', 'Romero', 231312312, 22, 'Telefono', '2322413', 'Calle Lopez', 823, 'Manza', 77, 'Casa', 18, Altura=1.83, Peso=105, CFavorito='Azul', Auto='Citroen', Modelo=2021)
+persona3.mostrar_detalle()
