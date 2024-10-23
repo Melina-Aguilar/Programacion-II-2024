6183ac2a35441db92e4ba72c520fb7664a12fce5
Melina Aguilar <aguilarmelinaelizabeth@gmail.com>
2024-10-22 21:52:59 -0300

    Agrego clase 10 PortfolioVuejs

--- PortfolioVuejs/src/App.vue
+++ PortfolioVuejs/src/App.vue
@@ -1,45 +1,60 @@
-<script setup> // setup: indica que se va a ejecutar solamente el codigo que se encuentra dentro de este script
-  import NavBar from './components/icons/NavBar.vue';
-  import DatosPersonales from './components/DatosPersonales.vue';
-  import Educacion from './components/EducacionComponente.vue';
-  import ExperienciaComponente from './components/ExperienciaComponente.vue';
-  import ProyectosComponente from './components/ProyectosComponente.vue';
+<script setup>
+import NavBar from './components/NavBar.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
+import Educacion from './components/EducacionComponente.vue';
+import ExperienciaComponente from './components/ExperienciaComponente.vue';
+import ProyectosComponente from './components/ProyectosComponente.vue';
+import HabilidadesComponente from './components/HabilidadesComponente.vue';
+import InteresesComponente from './components/InteresesComponente.vue';
 </script>
 
 <template>
-
-  <header>
-    <!-- Barra de Navegacion -->
-     <NavBar />
-
-    <!-- Componente Datos Persoanles-->
-      <DatosPersonales />
-  </header>
-
-  <main>
-    <!-- El estilo css de section se encuentra en base.css-->
-      <section id="educacion"><h2>Educacion - Cursos</h2>
-      <!--Componente Educacion-->
-      <Educacion/>
-      </section>
-
-      <section id="experiencia"><h2>Experiencia</h2>
-      <!--Componente Experiencia-->
-      <ExperienciaComponente/>
-      </section>
-
-      <section id="proyectos"><h2>Proyectos</h2>
-      <!--Componente Proyectos-->
-      <ProyectosComponente />
-      </section>
-
-      <section id="habilidades"><h2>Habilidades</h2></section>
-      <section id="intereses"><h2>Intereses</h2></section>
-  </main>
-  
-
+    <header id="top">
+        <!--Barra de Navegacion-->
+        <NavBar />
+        <!--Componente Datos Personales-->
+        <DatosPersonales />
+    </header>
+    <main>
+        <!--el estilo css de section se encuentra en base.css-->
+        <section id="educacion">
+            <h2>Educación - Cursos</h2>
+            <!--Componente Educación-->
+            <Educacion />
+        </section>
+        <section id="experiencia">
+            <h2>Experiencia</h2>
+            <!--Componente Experiencia-->
+            <ExperienciaComponente />
+        </section>
+        <section id="proyectos">
+            <h2>Proyectos</h2>
+            <!--Componente Proyectos-->
+            <ProyectosComponente />
+        </section>
+        <section id="habilidades">
+            <!--Componente Habilidades-->
+            <h2>Habilidades</h2>
+            <HabilidadesComponente />
+        </section>
+        <section id="intereses">
+            <!--Componente Intereses-->
+            <h2>Intereses</h2>
+            <InteresesComponente />
+        </section>
+    </main>
+    <footer>
+        <a href="#top">Inicio</a>
+        <p>© 2024 Melina Aguilar - Portafolio web - tutorial UTN FRSR</p>
+    </footer>
 </template>
 
 <style scoped>
-
+  footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+    font-size: 1.3rem;
+  }
 </style>
